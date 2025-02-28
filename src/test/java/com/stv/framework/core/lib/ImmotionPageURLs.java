dd1945426257e311876fa4eca928eac4cb2fc250
n04nik <85937098+n04nik@users.noreply.github.com>
2025-02-28 11:44:16 +0300

    Update ImmotionPageURLs.java
    commit

--- src/test/java/com/stv/framework/core/lib/ImmotionPageURLs.java
+++ src/test/java/com/stv/framework/core/lib/ImmotionPageURLs.java
@@ -2,6 +2,7 @@ package com.stv.framework.core.lib;
 
 public class ImmotionPageURLs {
     public static final String START_URL = "https://www.inmotionhosting.com/";
+    public static final String LOGIN_PAGE_URL = "https://secure1.inmotionhosting.com/index/login";
     public static final String FORGOT_PASSWORD_URL = "https://secure1.inmotionhosting.com/index/forgot";
     public static final String NEED_HELP_URL = "https://www.inmotionhosting.com/support/domain-names/dns-nameserver-changes/how-do-i-make-custom-dns-records/";
 }

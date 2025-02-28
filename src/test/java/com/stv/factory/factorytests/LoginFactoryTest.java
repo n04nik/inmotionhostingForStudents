410a5ee0c84163239a0760c97ac58555ae1e78e8
n04nik <85937098+n04nik@users.noreply.github.com>
2025-02-28 11:42:03 +0300

    Update LoginFactoryTest.java
    commit

--- src/test/java/com/stv/factory/factorytests/LoginFactoryTest.java
+++ src/test/java/com/stv/factory/factorytests/LoginFactoryTest.java
@@ -5,10 +5,8 @@ import com.stv.factory.factorypages.MainFactoryPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
+import static com.stv.framework.core.lib.ImmotionPageURLs.*;
 
-import static com.stv.framework.core.lib.ImmotionPageURLs.FORGOT_PASSWORD_URL;
-import static com.stv.framework.core.lib.ImmotionPageURLs.NEED_HELP_URL;
 
 public class LoginFactoryTest extends BasicFactoryTest {
 
@@ -29,9 +27,7 @@ public class LoginFactoryTest extends BasicFactoryTest {
     @Test(description = "Verify that clicking the 'Need Help' link redirects to the correct help page URL")
 
     public void testClickNeedHelpLink() {
-        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        setUp();
-        mainFactoryPage.clickLogin();
+
         loginFactoryPage.clickNeedHelpLink();
         String currentUrl = getDriver().getCurrentUrl();
         Assert.assertEquals(NEED_HELP_URL, currentUrl);
@@ -44,6 +40,9 @@ public class LoginFactoryTest extends BasicFactoryTest {
         loginFactoryPage.clickForgotPasswordLink();
         String currentUrlPassword = getDriver().getCurrentUrl();
         Assert.assertEquals(FORGOT_PASSWORD_URL, currentUrlPassword);
+        getDriver().navigate().back();
+        String currentUrlAfterBack = getDriver().getCurrentUrl();
+        Assert.assertEquals(LOGIN_PAGE_URL, currentUrlAfterBack);
     }
 
 }
